d89dc9f1e7796e31e4ca487134ddf5d4e7b617b9
Klymovych Oleksandr <warvar777@gmail.com>
2025-01-26 17:54:46 +0100

    Notes for Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './components/test/test.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'interview';
+  // Message from parent component
+  messageFromParent = 'Hello from parent component';
+
+  // Message from child component
+  messageFromChild = '';
+
+  getMessageFromChild(message: string) {
+    this.messageFromChild = message;
+  }
 }
--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './test.component.html',
-  styleUrl: './test.component.scss'
+  styleUrl: './test.component.scss',
 })
 export class TestComponent {
+  //* Property interpolation
+  title = 'interview';
+  firstName = 'John';
+  lastName = 'Doe';
+  //*---------------------------------------------------------------------------------
 
+  //* Property binding
+  isEnable: boolean = true;
+  isActivated: boolean = true;
+  isDisabled: boolean = false;
+  //*---------------------------------------------------------------------------------
+
+  //* Template string (Interpolation)
+  getFullName() {
+    return `My name is ${this.firstName} ${this.lastName}`;
+  }
+  //*---------------------------------------------------------------------------------
+
+  //* Event binding
+  onClick() {
+    this.isActivated = !this.isActivated;
+    this.isDisabled = !this.isDisabled;
+  }
+  //*---------------------------------------------------------------------------------
+
+  //* Two-way data binding
+  userName: string = '';
+  //*---------------------------------------------------------------------------------
+
+  //* Message from parent component
+  @Input() childMessage: string = '';
+  //*---------------------------------------------------------------------------------
+
+  //* Message to parent component
+  @Output() messageFromChild = new EventEmitter<string>();
+
+  sendMessageToParent() {
+    this.messageFromChild.emit('Hello from child component');
+  }
+  //*---------------------------------------------------------------------------------
 }
